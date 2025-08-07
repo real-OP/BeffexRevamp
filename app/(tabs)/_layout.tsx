0d600d19402cbb40b95271dd72f78214622ab615
realOP <sonihritvik77@gmail.com>
2025-08-07 16:43:05 +0530

    complete tab-bar with icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,16 @@
+import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="home" options={{title: "home"}} />
-      <Tabs.Screen name="progress" options={{title: "progress"}} />
-      <Tabs.Screen name="settings" options={{title: "settings"}} />
+    <Tabs screenOptions={{tabBarActiveTintColor:"black"}}>
+      <Tabs.Screen name="home" options={{title: "Home", tabBarIcon: ()=>(<FontAwesome5 name="home" size={24} color="black" />)}} />
+      <Tabs.Screen name="addtask" options={{title: "Add", tabBarIcon: ()=>(<Entypo name="add-to-list" size={24} color="black" />)}} />
+      <Tabs.Screen name="progress" options={{title: "Progress" , tabBarIcon: ()=>(<MaterialCommunityIcons name="progress-star" size={24} color="black" />)}} />
+      <Tabs.Screen name="settings" options={{title: "Settings" , tabBarIcon: ()=>(<Ionicons name="settings-sharp" size={24} color="black" />)}} />
     </Tabs>
   )
 }
