486d7a31f60b31bc1d8f149072098ca440ffe981
Hritvik <sonihritvik77@gmail.com>
2025-09-30 22:08:07 +0530

    added useeffects

--- context/TaskContext.jsx
+++ context/TaskContext.jsx
@@ -1,4 +1,5 @@
-import { createContext , useState ,  useCallback } from "react";
+import { createContext , useState ,  useCallback , useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const TaskContext = createContext({
     tasks: [],
@@ -7,8 +8,46 @@ export const TaskContext = createContext({
     deleteTask: (taskId) => {},
 });
 
+
+
 export const TaskProvider = ({children})=>{
     const [tasks , setTasks] = useState([]);
+
+
+    // Load Tasks from AsyncStorage on mount
+    useEffect(() => {
+
+        const loadTasks = async () => {
+            try{
+                const storedTasks = await AsyncStorage.getItem("tasks");
+                if(storedTasks){
+                    setTasks(JSON.parse(storedTasks));
+                }
+            }
+            catch(e){
+                console.error("Failed to load tasks from storage", e);
+            }
+        }
+        loadTasks();
+
+    },[])
+
+    // Save Tasks to AsyncStorage whenever tasks change
+
+    useEffect( () => {
+        const saveTasks = async () =>{
+            try{
+                await AsyncStorage.setItem("tasks" , JSON.stringify(tasks));
+            }
+            catch(e){
+                console.error("Failed to save tasks to storage", e);
+            }
+        }
+        if(tasks.length > 0 ) saveTasks();
+
+
+    },[tasks]);
+
 
     const addTask = useCallback((task)=>{
         setTasks((prev)=>[...prev, { id: Date.now() , completed: false , ...task}]);
